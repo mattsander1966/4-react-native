5445624ae2066358dc1a00e14aa7fc201ea741b5
Matt Sander <msander@mintventures.net>
2020-08-10 12:57:14 -0400

    updated contactComponent

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,58 +1,30 @@
 import React, { Component } from 'react';
-import { ScrollView, Text, FlatList } from 'react-native';
-import { Card, ListItem } from 'react-native-elements';
-import { render } from 'react-dom'; 
-import { COMMENTS } from '../shared/comments';
+import { ScrollView, Text, } from 'react-native';
+import { Card, } from 'react-native-elements';
 
-// function Contact() {
-//     return (
-//         <Card title='Contact Information' wrapperStyle={{margin: 20}}>
-//             <Text style={{marginBottom: 10}}>
-//                             1 Nucamp Way
-//                             Seattle, WA 98001
-//                             U.S.A.
 
-//                             Phone: 1-206-555-1234
-//                             Email: campsites@nucamp.co
-//             </Text>
-//         </Card>
-//     );
 
-    class Contact extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                comments: COMMENTS
-            };
+class Contact extends Component {
+       
+        static navigationOptions = {
+            title: 'Contact Us'
         }
 
-        state navitationOptions ={
-            title: 'Contact Us'
-        };
-
         render() {
-            const renderComments = ({item}) => {
-                return (
-                    <ListItem
-
-                    />
-                );
-            };
-
             return (
                 <ScrollView>
-                    <Comment />
                     <Card
-                        title="Contact Info">
-                        <FlatList
-                            data={this.state.comments}
-                            renderItem={}
-                            keyExtractor={item=>item.id.toString()}
-                        />
+                        title='Contact Information'
+                        wrapperStyle={{margin: 20}}>
+                        <Text>1 Nucamp Way</Text>
+                        <Text>Seattle, WA 98001</Text> 
+                        <Text stuyle={{marginBottom: 10}}>USA</Text> 
+                        <Text>Phone: 206-555-9874</Text> 
+                        <Text>email: info@nucamp.co</Text>     
                     </Card>
                 </ScrollView>
-        );
+            );
+        };
     }
-}
 
 export default Contact;
