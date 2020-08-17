a61fae46d2759db9cc98f7fc91d47b4a65ba3653
Matt Sander <msander@mintventures.net>
2020-08-17 12:48:32 -0400

    swipeout

--- Redux/ActionCreators.js
+++ Redux/ActionCreators.js
@@ -31,6 +31,19 @@ export const addComments = (comments) => ({
     payload: comments
 });
 
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text
+    };
+    newComment.date = new Date().toISOString();
+
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+}
 export const fetchCampsites = () => dispatch => {
 
     dispatch(campsitesLoading());
@@ -156,4 +169,7 @@ export const addFavorite = campsiteId => ({
     payload: campsiteId
 });
 
-// export const postComment = 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+});
